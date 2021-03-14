af05ca8bc634dc7da3a99a12e054517ee1f8e16b
Javo2804 <j.z.avello@gmail.com>
2021-03-14 01:00:23 -0300

    dar Canvas experimental

---
+++ src/background.js
@@ -0,0 +1,11 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.create({ url: chrome.runtime.getURL("actualizacion.html") });
+});
+
+chrome.runtime.onInstalled.addListener(function(object) {
+    if (object.reason == "install") {
+        chrome.storage.sync.set({
+            setcanvas: false,
+        });
+    }    
+});
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,3 +1,22 @@
 var manifestData = chrome.runtime.getManifest();
 var version = manifestData.version;
 document.getElementById("version").innerHTML = "v"+version
+
+
+// document.querySelector('#ir-config').addEventListener('click', function() {
+//   if (chrome.runtime.openOptionsPage) {
+//     chrome.runtime.openOptionsPage();
+//   } else {
+//     window.open(chrome.runtime.getURL('config.html'));
+//   }
+// });
+
+// document.querySelector('#openconfig').addEventListener('click', function() {
+//   var x = document.querySelector('#config1');
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//     document.getElementById('')
+//   } else {
+//     x.style.display = "none";
+//   }
+// });
